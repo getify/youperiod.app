d62e549a20f0f9365bc99e5efa1955419a2481cc
Alan R <1534554+closuresandcoffee@users.noreply.github.com>
2022-07-13 18:22:06 -0500

    Add utility methods to assist in checking for browser API support

---
+++ web/js/browser-support.js
@@ -0,0 +1,78 @@
+function webAssemblySupported() {
+	return "WebAssembly" in window
+		&& typeof window.WebAssembly.instantiate === "function";
+}
+
+function serviceWorkersSupported() {
+	return "serviceWorker" in navigator;
+}
+
+function webWorkersSupported() {
+	return "Worker" in window;
+}
+
+function indexedDbSupported() {
+	return "indexedDB" in window
+		&& typeof window.indexedDB.open === "function";
+}
+
+function sessionStorageSupported() {
+	var localStorageAvailable = "localStorage" in window
+		&& typeof window.localStorage.setItem === "function";
+
+	var sessionStorageAvailable = "sessionStorage" in window
+		&& typeof window.sessionStorage.setItem === "function";
+
+	return localStorageAvailable && sessionStorageAvailable;
+}
+
+function webCryptoSupported() {
+	return "crypto" in window
+		&& typeof window.crypto.randomUUID === "function";
+}
+
+function textEncodeDecodeSupported() {
+	return "TextEncoder" in window && "TextDecoder" in window;
+}
+
+function storeManagerPersistSupported() {
+	return "storage" in window.navigator
+		&& typeof window.navigator.storage.persist === "function";
+}
+
+function notificationApiSupported() {
+	return "Notification" in window;
+}
+
+function credentialsContainerSupported() {
+	return "credentials" in window.navigator
+		&& typeof window.navigator.credentials.get === "function";
+}
+
+function getUserMediaSupported() {
+	return "mediaDevices" in window.navigator
+		&& typeof window.navigator.mediaDevices.getUserMedia === "function";
+}
+
+function minimumFeaturesSupported() {
+	return (
+		webAssemblySupported()
+		&& serviceWorkersSupported()
+		&& webWorkersSupported()
+		&& indexedDbSupported()
+		&& sessionStorageSupported()
+		&& webCryptoSupported()
+		&& textEncodeDecodeSupported()
+		&& storeManagerPersistSupported()
+	);
+}
+
+function optionalFeaturesSupported() {
+	return (
+		notificationApiSupported()
+		&& credentialsContainerSupported()
+		&& getUserMediaSupported()
+	);
+}
+
+export { minimumFeaturesSupported, optionalFeaturesSupported };
